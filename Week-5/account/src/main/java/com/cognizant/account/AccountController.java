2a6778492508793152f33bfd98761d72339e234b
srisakthi-2806 <153521854+srisakthi-2806@users.noreply.github.com>
2025-07-19 10:19:17 +0530

    Add files via upload

---
+++ Week-5/account/src/main/java/com/cognizant/account/AccountController.java
@@ -0,0 +1,18 @@
+package com.cognizant.account;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Map<String, Object> getAccount(@PathVariable String number) {
+        Map<String, Object> account = new HashMap<>();
+        account.put("number", number);
+        account.put("type", "savings");
+        account.put("balance", 234343);
+        return account;
+    }
+}
